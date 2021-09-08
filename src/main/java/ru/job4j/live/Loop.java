20863521451cf3a5397348b8950a4afae770938a
maks-debug <belyakov_maks@internet.ru>
2021-09-08 19:22:07 +0300

    5.9.1. Генерация конструкций в IDEA [#279134]

---
+++ src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,9 @@
+package ru.job4j.live;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int index = 0; index < 11; index++) {
+            System.out.println(index);
+        }
+    }
+}
