75185d1820fa2272c922fd7f51caa69f250ff5da
maks-debug <belyakov_maks@internet.ru>
2021-09-27 16:08:31 +0300

    Исправленние нарушений по правилам Job4j CQP.

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,23 +2,26 @@ package ru.job4j.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
-//        int one = 1;
-//        int two = 2;
-//        int four = 4;
-//        int five = 5;
-//        int six = 6;
-//        int size = 10;
-//        size = 100;
-//        size = size - 5;
-//        int onePlusTwo = one + two;
-//        int sixDivTwo = six/two;
-//        int fiveMinusTwo = five - two;
-//        int fourTimeTwo = four * two;
-//        System.out.println(onePlusTwo);
-//        System.out.println(sixDivTwo);
-//        System.out.println(fiveMinusTwo);
-//        System.out.println(fourTimeTwo);
-//        System.out.println(size);
+/**
+ * The first option for completing the task
+ *         int one = 1;
+ *         int two = 2;
+ *         int four = 4;
+ *         int five = 5;
+ *         int six = 6;
+ *         int size = 10;
+ *         size = 100;
+ *         size = size - 5;
+ *         int onePlusTwo = one + two;
+ *         int sixDivTwo = six/two;
+ *         int fiveMinusTwo = five - two;
+ *         int fourTimeTwo = four * two;
+ *         System.out.println(onePlusTwo);
+ *         System.out.println(sixDivTwo);
+ *         System.out.println(fiveMinusTwo);
+ *         System.out.println(fourTimeTwo);
+ *         System.out.println(size);
+ */
         Calculator.plus(100, 500);
         Calculator.plus(4, 2);
         Calculator.plus(3, 5);
--- src/main/java/ru/job4j/loop/ModWithLoop.java
+++ src/main/java/ru/job4j/loop/ModWithLoop.java
@@ -2,17 +2,23 @@ package ru.job4j.loop;
 
 public class ModWithLoop {
     public static int mod(int n, int d) {
-//        int result = 0;
+        /**
+         * The first option for completing the task
+         *  int result = 0;
+         *         while (n > d) {
+         *             n -= d;
+         *             result++;
+         *         }
+         *         if (n != d) {
+         *             result = n;
+         *         } else {
+         *             result = 0;
+         *         }
+         *         return result;
+         */
         while (n > d) {
             n -= d;
-//            result++;
         }
-
-//        if (n != d) {
-//            result = n;
-//        } else {
-//            result = 0;
-//        }
         return n != d ? n : 0;
     }
 
